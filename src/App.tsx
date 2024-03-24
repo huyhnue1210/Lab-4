b624291b90f84787f6ed8777a778f0394b57db0b
huyhnue1210 <huydqph41185@fpt.edu.vn>
2024-03-24 23:54:45 +0700

    gd1

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import ProductPage from './pages/ProductPage';
+import Home from './pages/HomePage';
+import Login from './pages/LoginPage';
+import Register from './pages/RegisterPage';
+import Notfound from './pages/NotfoundPage';
+import ProductDetail from './pages/ProductDetailPage/ProductDetailPage';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App: React.FC = () => {
+	return (
+		<div>
+			<Header />
+			<main id="main" className="container">
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/shop" element={<ProductPage />} />
+					<Route path="/shop/:productId" element={<ProductDetail />} />
+					<Route path="/login" element={<Login />} />
+					<Route path="/register" element={<Register />} />
+					<Route path="*" element={<Notfound />} />
+				</Routes>
+			</main>
+			<Footer />
+		</div>
+	);
+};
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+export default App;
---
+++ src/pages/NotfoundPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const NotfoundPage = (props: Props) => {
+  return (
+    <div>NotfoundPage</div>
+  )
+}
+
+export default NotfoundPage
